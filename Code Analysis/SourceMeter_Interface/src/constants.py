6762ab0c436ecb157c480a672289887b68330a37
Damian Najera <dgnajera2@miners.utep.edu>
2018-10-17 13:52:34 -0600

    Added constants file for easier reconfiguration.

---
+++ Code Analysis/SourceMeter_Interface/src/constants.py
@@ -0,0 +1,13 @@
+import os
+
+SOURCE_METER_DIR_NAME = "SourceMeter-8.2.0-x64-linux"
+SOURCE_METER_JAVA_PATH = os.path.join(os.getcwd(), "..", SOURCE_METER_DIR_NAME, "Java", "SourceMeterJava")
+SOURCE_METER_PYTHON_PATH = os.path.join(os.getcwd(), "..", SOURCE_METER_DIR_NAME, "Python", "SourceMeterPython")
+
+CLASS_KEEP_COL = ['Name', 'LOC', 'CD', 'CBO', 'NOI']
+METHOD_KEEP_COL = ['Name', 'Path', 'LOC', 'NUMPAR', 'CD']
+
+RESULTS_DIR = os.path.join(os.getcwd(), "..", "Results")
+
+JAVA_SAMPLE_PROJ_DIR = os.path.join(os.getcwd(), "..", SOURCE_METER_DIR_NAME, "Java", "Demo", "apache-log4j-1.2.17")
+PYTHON_SAMPLE_PROJ_DIR = os.path.join(os.getcwd(), "..", SOURCE_METER_DIR_NAME, "Python", "Demo", "ceilometer")
