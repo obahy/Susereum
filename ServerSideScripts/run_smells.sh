18140fbe5473f14938ae6e221b7c00dba4ef84ab
Suseruim <susereum@gmail.com>
2018-11-10 13:33:30 -0700

    Added commit handler

---
+++ ServerSideScripts/run_smells.sh
@@ -0,0 +1,3 @@
+SAWTOOTH_HOME=/home/practicum2018/.sawtooth_projects/.TestProject_123
+cd $SAWTOOTH_HOME
+python3 families/code-smell/client/code_smell.py default --url http://127.0.0.1:1025 --path $SAWTOOTH_HOME
