c1f034f03f277822c6963799dfbfdb11453bddc9
Abel Gomez <abelgomezr45@gmail.com>
2018-11-16 16:42:26 -0800

    add christian

--- Sawtooth/families/health/client/health_client.py
+++ Sawtooth/families/health/client/health_client.py
@@ -126,6 +126,7 @@ class HealthClient:
         except IOError as error:
             raise HealthException("Unable to open configuration file {}".format(error))
 
+        ## TODO: add christian code
         repo_path = repo_path.replace('\n', '') + '/CodeAnalysis/SourceMeter_Interface/src/sourceMeterWrapper.py'
         csv_path = subprocess.check_output( ['python', repo_path, github_url, sawtooth_home]).decode('utf-8')
         csv_path = csv_path[csv_path.rfind('OK\n')+4:-4]#check if "OK\n" is in project name or read from file
