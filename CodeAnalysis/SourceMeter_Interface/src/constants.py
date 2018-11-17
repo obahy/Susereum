d7321a48678e9cc60de8bd061309cd79102e337f
Damian Najera <dgnajera2@miners.utep.edu>
2018-11-17 13:45:00 -0700

    Removed OS-specific constants. Modified paths for consistency. Added TMP_DIR constant that can be changed.

--- CodeAnalysis/SourceMeter_Interface/src/constants.py
+++ CodeAnalysis/SourceMeter_Interface/src/constants.py
@@ -1,24 +1,15 @@
 import os
 
-# OS-specific constants
-if os.name == 'nt':
-    POSIX = False
-    DIR_SEPARATOR = "\\"
-    SOURCE_METER_DIR_NAME = "SourceMeter-8.2.0-x64-windows"
-else:
-    POSIX = True
-    DIR_SEPARATOR = "/"
-    SOURCE_METER_DIR_NAME = "SourceMeter-8.2.0-x64-linux"
+SOURCE_METER_DIR_NAME = "SourceMeter-8.2.0-x64-linux"
 
-folder = os.path.dirname(__file__)
-SOURCE_METER_JAVA_PATH = os.path.join(folder, "..", SOURCE_METER_DIR_NAME, "Java", "SourceMeterJava")
-SOURCE_METER_PYTHON_PATH = os.path.join(folder, "..", SOURCE_METER_DIR_NAME, "Python", "SourceMeterPython")
+FOLDER = os.path.dirname(os.path.abspath(__file__))
+SOURCE_METER_JAVA_PATH = os.path.join(FOLDER, "..", SOURCE_METER_DIR_NAME, "Java", "SourceMeterJava")
+SOURCE_METER_PYTHON_PATH = os.path.join(FOLDER, "..", SOURCE_METER_DIR_NAME, "Python", "SourceMeterPython")
 
 CLASS_KEEP_COL = ['Name', 'LOC', 'CD', 'CBO', 'NOI']
 METHOD_KEEP_COL = ['Name', 'Path', 'LOC', 'NUMPAR', 'CD']
 
-JAVA_SAMPLE_PROJ_DIR = os.path.join(folder, "..", SOURCE_METER_DIR_NAME, "Java", "Demo", "apache-log4j-1.2.17")
-PYTHON_SAMPLE_PROJ_DIR = os.path.join(folder, "..", SOURCE_METER_DIR_NAME, "Python", "Demo", "ceilometer")
+CLEAN_UP_SM_FILES = True                     # Delete Source Meter-created metrics after script execution?
+CLEAN_UP_REPO_FILES = True                   # Delete GitHub repo files that are cloned by sourceMeterWrapper.py?
 
-CLEAN_UP_SM_FILES = True                   # Delete Source Meter-created metrics after script execution?
-CLEAN_UP_REPO_FILES = True                 # Delete GitHub repo files that are cloned by sourceMeterWrapper.py?
+TMP_DIR = os.path.join(FOLDER, "..", 'tmp')  # The temp directory where projects can be cloned.
