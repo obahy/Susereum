243648bf5c618a091c8c35bbfaa71295feefdf6f
Suseruim <susereum@gmail.com>
2018-11-19 23:52:42 -0700

    Added README, renamed to gitApiInterface, added more comments

--- GitHub App/sampleSender.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-data = json.dumps({'sender': 'Sawtooth', 'name': 'Martin', 'password': 'MyPassword', 'age': 23})
-r = requests.post('http://localhost:3000', data=data)
-print("Payload: " + r.text)
