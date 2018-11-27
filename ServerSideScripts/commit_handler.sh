6f70d627da19183e0dc48a65ba332a89385c8823
Suseruim <susereum@gmail.com>
2018-11-26 20:30:20 -0700

    pre fixing path merge

--- ServerSideScripts/commit_handler.sh
+++ ServerSideScripts/commit_handler.sh
@@ -24,21 +24,22 @@ else
 fi
 rm mycron
 echo "$cron_cmd $(date)" > /commit_handler_cron.txt
-
+#send a new comit
+#get chain ports
+echo 'sending new commit'
+Dir=$(
+cd $(dirname "$0")
+pwd
+)
+readarray ports < $Dir/map/$REPOID
+export SAWTOOTH_HOME="$HOME/.sawtooth_projects/.$(echo $NAME)_$(echo $REPOID)"
+cd $SAWTOOTH_HOME
+#push to chain the commit
+api=$(echo ${ports[2]} | tr -d '\n')
 #if the client did the health
 health_done=`sawtooth transaction list --url http://127.0.0.1:$api | grep "health" | grep $COMMIT_URL`
 if [ -z "$health_done" ] ;then
-	#send a new comit
-	#get chain ports
-	Dir=$(
-	cd $(dirname "$0")
-	pwd
-	)
-	readarray ports < $Dir/map/$REPOID
-	export SAWTOOTH_HOME="$HOME/.sawtooth_projects/.$(echo $NAME)_$(echo $REPOID)"
-	cd $SAWTOOTH_HOME
-	#push to chain the commit
-	api=$(echo ${ports[2]} | tr -d '\n')
+	
 	#get random client public key 
 	transaction_id=`sawtooth transaction list --url http://127.0.0.1:$api | tail -n +2 | shuf -n 1 | awk '{print $1;}'`
 	key=`sawtooth transaction show $transaction_id  --url http://127.0.0.1:$api | grep signer_public_key | awk '{print $2;}'`
